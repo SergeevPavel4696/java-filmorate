6669b87fa57b4bf0c8432b8388d2bd637dbcfa93
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-08-16 23:43:41 +0500

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,9 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
@@ -14,5 +12,5 @@ public class Film {
     private String name;
     private String description;
     private LocalDate releaseDate;
-    private Duration duration;
+    private Integer duration;
 }

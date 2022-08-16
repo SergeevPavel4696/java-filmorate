e4f67111531bd52ecdf91e2654aa1dd95405ca01
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-08-16 21:06:11 +0500

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -50,9 +50,10 @@ public class UserController {
     @PutMapping(value = "/users")
     public User updateUser(@RequestBody User user) {
         isUser(user);
-        if (users.containsKey(user.getId())) {
-            users.put(user.getId(), user);
+        if (!users.containsKey(user.getId())) {
+            throw new UserRequestException();
         }
+        users.put(user.getId(), user);
         return user;
     }
 

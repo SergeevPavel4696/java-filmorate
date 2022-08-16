65b37f3d295bbfe2853d46d8896b3b2699212017
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-08-16 21:06:34 +0500

    Update UserRequestException.java

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserRequestException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserRequestException.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 public class UserRequestException extends RuntimeException {
+    public UserRequestException() {}
+
     public UserRequestException(String message) {
         super(message);
     }

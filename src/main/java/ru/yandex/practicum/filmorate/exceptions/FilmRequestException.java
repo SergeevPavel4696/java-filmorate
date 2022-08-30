bffcb448854c9b8bef873c07d3ad94009587b980
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-08-30 23:39:11 +0500

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmRequestException.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class FilmRequestException extends RuntimeException {
-    public FilmRequestException() {}
-
-    public FilmRequestException(String message) {
-        super(message);
-    }
-}

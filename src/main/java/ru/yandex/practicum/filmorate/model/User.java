65781ce9728c7bfaeb3b3a7bfed9b6bcafafcb1b
SergeevPavel4696 <97850194+SergeevPavel4696@users.noreply.github.com>
2022-09-21 11:00:34 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Builder
+public class User {
+    private Integer id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
+    private Set<Integer> friends;
+}
